5c7bef550669256afbac7a09ea34ee40e2c32094
Jamie Goodnight <goodnightlambda@gmail.com>
2019-02-27 14:50:18 -0500

    Created friends, created Friends, added friends to my state as friends. Friends. Initial commit.

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+class Friends extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            friends=[],
+            error=''
+        };
+    }
+    componentDidMount(){
+        axios
+            .get('http://localhost:5000/friends')
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    friends: res.data
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({
+                    error: err
+                })
+            })
+    }
+    render(){
+        return (    
+            <div>
+                {this.state.friends.map(x => (
+                    <div key={x.id}>
+                        <h3>x.name</h3>
+                        <ul>
+                            <li>{x.age}</li>
+                            <li>{x.email}</li>
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+
+export default Friends;
